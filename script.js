4b1fd237c7c4400d640022d828bee50bf41909d7
Stavros <101610510@student.swin.edu.au>
2021-04-21 10:06:42 +1000

    clear input after adding item

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function AddItem() {
-    var item = document.createElement("P");
-    var text = document.getElementById("text").value;
-    item.innerHTML = text;
-    document.getElementById("list").appendChild(item);
+    var item = document.createElement("P");                     //create a paragraph element, store it in a variable
+    var input = document.getElementById("text");                //store the input box as a variable
+    var text = input.value;                                     //store the value of the input box as a variable
+
+    item.innerHTML = text;                                      //set the innerhtml of paragraph element
+    document.getElementById("list").appendChild(item);          //append the paragraph element to the element
+    input.value = '';                                           //clear the input box
 }
